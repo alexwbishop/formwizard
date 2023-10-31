c9aa2733aeed230e21e6fb799106971326d343bb
alexwbishop <incyde@riseup.net>
2023-10-31 11:21:48 -0500

    workinprog1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Purpose: Automate the process of filling out Delaware Change of Agent forms (Corp, LLC, LP), Domestic and Foreign.
 
 # Import stuff
+from base_form import BaseForm
 import json
 from reportlab.pdfgen import canvas
 import PyPDF2
@@ -28,44 +29,105 @@ def populate_form(form_template_path, output_pdf_path, field_coordinates, field_
 
     c.save()
 
+# Function to merge text PDF onto blank form
+from PyPDF2 import PdfReader, PdfWriter
+
+def merge_pdfs(form_pdf_path, text_pdf_path, output_pdf_path):
+    pdf_reader_form = PdfReader(open(form_pdf_path, 'rb'))
+    pdf_reader_text = PdfReader(open(text_pdf_path, 'rb'))
+    
+    pdf_writer = PdfWriter()
+    
+    page_form = pdf_reader_form.pages[0]
+    page_text = pdf_reader_text.pages[0]
+    
+    page_form.merge_page(page_text)
+    pdf_writer.add_page(page_form)
+    
+    with open(output_pdf_path, 'wb') as f:
+        pdf_writer.write(f)
+
+
 # Load JSON configuration
 with open('field_coordinates.json', 'r') as f:
     form_config = json.load(f)
 
 # PHASE 1 = Basic command line prompt usage:
 
-# Function to get user input for form type and details
-def get_user_input():
-    print("Select the entity type:")
-    entity_type = input("1: LLC\n2: Corp\n3: LP\n")
-    
-    print("Select Domestic or Foreign:")
-    dom_for = input("1: Domestic\n2: Foreign\n")
-    
-    print("Confirm the agent:")
-    agent_choice = input("1: CT (The Corporation Trust Company)\n2: NRAI (National Registered Agents, Inc.)\n")
-    
-    # Map user input to actual values
-    entity_map = {'1': 'LLC', '2': 'Corp', '3': 'LP'}
-    dom_for_map = {'1': 'Dom', '2': 'For'}
-    agent_map = {'1': 'The Corporation Trust Company', '2': 'National Registered Agents, Inc.'}
-    
-    entity_type = entity_map.get(entity_type, 'LLC')
-    dom_for = dom_for_map.get(dom_for, 'Dom')
-    agent_name = agent_map.get(agent_choice, 'The Corporation Trust Company')
-    
-    return entity_type, dom_for, agent_name
+# Greet and Confirm that User is Alex
+user_name = input("Hello! Are you Alex? (Y/N): ")
+if user_name.lower() == 'y':
+    password = input("Please enter your password: ")
+    if password == 'daliarules':
+        print("Welcome, Alex!")
+    else:
+        print("Incorrect password. Exiting.")
+        exit()
+else:
+    print("You are not authorized to use this program. Exiting.")
+    exit()
 
-# Get user input
-entity_type, dom_for, agent_name = get_user_input()
+# Ask for number of forms to complete
+num_forms = int(input("How many DE forms would you like to prepare for this session? (Up to 10): "))
+if num_forms > 10:
+    print("Sorry, you can only prepare up to 10 forms at a time.")
+    exit()
 
-# Construct form key based on user input (e.g., 'DE-LLC-Dom-COA')
-form_key = f"DE-{entity_type}-{dom_for}-COA"
+# Ask if we are to use CT or NRAI as Agent
+    agent_name = input(f"Confirm the agent to be designated: ")
+# Collect Signer's Name
+    signer_first_name = input("Enter the signer's first name: ")
+    signer_last_name = input("Enter the signer's last name: ")
+ # Confirm Signer's Name
+print(f"Signer's full name is {signer_first_name} {signer_last_name}. Is this correct? (Y/N): ")
+confirmation = input()
+if confirmation.lower() != 'y':
+    print("Please restart the session with the correct signer's name.")
+    exit()
 
-# Prompt user for remaining information
-entity_name = input("Enter the entity name: ")
-sig_conformed = input("Enter the conformed signature: ")
-signer_name = input("Enter the signer's name: ")
+# Initialize list to store form instances
+forms = []
+
+# Collect entity names, types, and residency
+for i in range(num_forms):
+    entity_name = input(f"Enter the name of entity {i+1}: ")
+    entity_type = input(f"Confirm the entity type for {entity_name} (LLC/Corp/LP): ")
+    dom_for = input(f"Is {entity_name} Domestic or Foreign to Delaware? (Y/N): ")
+
+    print(f"Entity info entered: {entity_name} - {entity_type} ({dom_for}). Is this correct? (Y/N): ")
+        confirmation = input()
+        if confirmation.lower() == 'y':
+            break
+
+# Create an instance of your form class and store the data
+form_instance = BaseForm(
+    entity_name=entity_name,
+    jurisdiction_instance=None,  # You'll need to set this
+    domestic_state=dom_for,
+    agent_name=agent_name,
+    agent_street1=None,  # You'll need to set this
+    # ... (and so on for all the other attributes)
+    user_id="Alex"  # Since you're the user
+)
+
+forms.append(form_instance)
+
+# Display list of entities & forms to be filled
+print("List of entities/forms to be filled in this session:")
+for i, form in enumerate(forms):
+    print(f"{i+1}) {form.entity_name} - {form.entity_type} - Filing Type (Change of Agent)")
+
+# Display signer's conformed signature
+conformed_signature = f"/s/{signer_first_name} {signer_last_name}"
+print(f"Conformed signature: {conformed_signature}")
+
+# Ask user to confirm Y/N to proceed. If N, quit program.
+print(f"All information obtained, ready to complete forms now. Proceed? (Y/N): ")
+confirmation = input()
+if confirmation.lower() != 'y':
+    print("Please restart the session with the correct information.")
+    exit()
+
 
 # Update form_data with user input
 form_data = {
@@ -77,5 +139,15 @@ form_data = {
 
 # Run the populate function on the form
 populate_form(f'StateForms/DE/{form_key}.pdf', f'StateForms/DE/output_{form_key}.pdf', form_config.get(form_key, {}), form_data)
+
+# Temporary text PDF path
+temp_text_pdf_path = f'completed_forms/temp/temp_text_{form_key}.pdf'
+
+# Populate form with text
+populate_form(f'StateForms/DE/{form_key}.pdf', temp_text_pdf_path, form_config.get(form_key, {}), form_data)
+
+# Merge the original form and text PDF
+merge_pdfs(f'StateForms/DE/{form_key}.pdf', temp_text_pdf_path, f'completed_forms/{entity_name}_-_{form_key}_Filled.pdf')
+
 
 # Done! For now...
