c34d87b889c6a20dc8179e62708760386e8f76c6
Alex Bishop <incyde@riseup.net>
2023-11-01 17:48:07 -0500

    Update session_utils.py

--- session_utils.py
+++ session_utils.py
@@ -12,9 +12,6 @@ from questions import CAQuestion, DEQuestion
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
-from PyPDF2 import PdfReader, PdfWriter
-from classes.BaseForm.base_form import BaseForm
-from classes.Jurisdiction.jurisdiction import Jurisdiction
 
 # functions
 
@@ -22,14 +19,5 @@ from classes.Jurisdiction.jurisdiction import Jurisdiction
 def generate_session_id():
     session_id = uuid.uuid4().hex.upper()[0:6]
     return session_id
-
-# Create a session_id for the form prep session
-if __name__ == "__main__":
-    session_id = generate_session_id()
-#  assign the user_id and timestamp to the session markers
-form_instance.user_id = user_id
-form_instance.session_id = session_id
-form_instance.session_timestamp = datetime.now()
-logging.info(f"Thank you for authenticating, {user_id}! \n Form prep session initialized. \n Username: {user.id} | Session ID: {session_id}") | Timestamp: {session_timestamp}")
 
 
