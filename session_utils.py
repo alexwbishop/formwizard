0ba89ce6706c730408da6e0a8481a5400a82d819
Alex Bishop <incyde@riseup.net>
2023-11-01 16:21:21 -0500

    refactor 1

--- session_utils.py
+++ session_utils.py
@@ -18,3 +18,18 @@ from classes.Jurisdiction.jurisdiction import Jurisdiction
 
 # functions
 
+# Define creation of session id#
+def generate_session_id():
+    session_id = uuid.uuid4().hex.upper()[0:6]
+    return session_id
+
+# Create a session_id for the form prep session
+if __name__ == "__main__":
+    session_id = generate_session_id()
+#  assign the user_id and timestamp to the session markers
+form_instance.user_id = user_id
+form_instance.session_id = session_id
+form_instance.session_timestamp = datetime.now()
+logging.info(f"Thank you for authenticating, {user_id}! \n Form prep session initialized. \n Username: {user.id} | Session ID: {session_id}") | Timestamp: {session_timestamp}")
+
+
