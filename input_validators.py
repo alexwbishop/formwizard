f6c5a2631a2a002dd3a7a10615b9a28bc8a808ef
Alex Bishop <incyde@riseup.net>
2023-11-01 15:17:54 -0500

    Create input_validators.py

---
+++ input_validators.py
@@ -0,0 +1,19 @@
+# input validators.py
+
+# Imports
+import re
+import json
+import os
+import PyPDF2
+import uuid
+import logging
+from questions import CAQuestion, DEQuestion
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
+from PyPDF2 import PdfReader, PdfWriter
+from classes.BaseForm.base_form import BaseForm
+from classes.Jurisdiction.jurisdiction import Jurisdiction
+
+# functions
+
