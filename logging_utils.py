c5db619e014fbc2ffd9c12bc5f7da1f9c694eb8e
Alex Bishop <incyde@riseup.net>
2023-11-01 16:21:13 -0500

    refactor 1

--- logging_utils.py
+++ logging_utils.py
@@ -28,3 +28,9 @@ try:
     logging.info(f"Current time: {current_time}")
 except Exception as e:
     logging.error(f"An error occurred: {e}")
+
+# Display a complete list of up to 10 entities & forms to be filled
+logging.info(f"List of entities/forms to be filled in this session: ")
+for i, form in enumerate(forms):
+    logging.info(f"{i+1}) form.entity_name - form.entity_type - form.filing_type")
+
