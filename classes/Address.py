f10e10b746f62ec332a36568e57df12e016b7b8f
Alex Bishop <incyde@riseup.net>
2023-11-02 14:06:49 -0500

    Update Address.py

--- classes/Address.py
+++ classes/Address.py
@@ -1,7 +1,8 @@
 # address.py
 class Address:
-    def __init__(self, street1, city, state, zip):
+    def __init__(self, street1, street2, city, state, county, zip):
         self.street1 = street1
+        self.street2 = street2
         self.city = city
         self.state = state
         self.zip = zip
