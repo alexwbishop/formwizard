c0950a21034fd1010f6c857243ac1f85630d6085
Alex Bishop <incyde@riseup.net>
2023-11-02 10:59:18 -0500

    Update config_utils.py

--- config_utils.py
+++ config_utils.py
@@ -4,17 +4,7 @@
 # Imports
 import re
 import json
-import os
-import PyPDF2
-import uuid
 import logging
-from questions import CAQuestion, DEQuestion
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.units import inch
-from reportlab.pdfgen import canvas
-from PyPDF2 import PdfReader, PdfWriter
-from classes.BaseForm.base_form import BaseForm
-from classes.Jurisdiction.jurisdiction import Jurisdiction
 
 # functions
 
