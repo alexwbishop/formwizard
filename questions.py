8241c8db5c87bc3ad49fa43276ab47262fcd806b
Alex Bishop <incyde@riseup.net>
2023-11-02 11:03:12 -0500

    Update questions.py

--- questions.py
+++ questions.py
@@ -15,17 +15,15 @@ def load_json_config(filename):
         return json.load(f)
 
 # JSON configuration function
-def main():
-    # Load configs
-    config = load_json_config("config.json")
-    form_config = load_json_config('field_coordinates.json')
-    VALID_STATES = config.get('VALID_STATES', [])
-    ENTITY_TYPES = config.get('ENTITY_TYPES', [])
-    FILING_TYPES = config.get('FILING_TYPES', [])
-    ALL_STATES = config.get('ALL STATES', [])
-    MAX_FORM_QUANTITY = config.get('MAX_FORM_QUANTITY', 10)
-    VALID_AGENT_NAMES = config.get('VALID_AGENT_NAMES', [])
-    DEFAULTS = config.get('DEFAULTS', [])
+config = load_json_config("config.json")
+form_config = load_json_config('field_coordinates.json')
+VALID_STATES = config.get('VALID_STATES', [])
+ENTITY_TYPES = config.get('ENTITY_TYPES', [])
+FILING_TYPES = config.get('FILING_TYPES', [])
+ALL_STATES = config.get('ALL STATES', [])
+MAX_FORM_QUANTITY = config.get('MAX_FORM_QUANTITY', 10)
+VALID_AGENT_NAMES = config.get('VALID_AGENT_NAMES', [])
+DEFAULTS = config.get('DEFAULTS', [])
 
 def get_confirmation(prompt):
     while True:
