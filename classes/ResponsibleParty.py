22f6c03d77246119d226fc10a3ec51dba50345aa
Alex Bishop <incyde@riseup.net>
2023-11-02 14:37:51 -0500

    Update ResponsibleParty.py

--- classes/ResponsibleParty.py
+++ classes/ResponsibleParty.py
@@ -4,8 +4,10 @@
 from address import Address
 
 class ResponsibleParty:
-    def __init__(self, first, mid, last, address: Address):
+    def __init__(self, first, mid, last, title, is_director, address: Address):
         self.first_name = first
         self.mid_name = mid
         self.last_name = last
         self.address = address
+        self.title = title
+        self.is_director = 
