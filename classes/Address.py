b72a545f0f6c66955bf6f825b5e490e1ac58854a
Alex Bishop <incyde@riseup.net>
2023-11-02 14:05:17 -0500

    Create Address.py

---
+++ classes/Address.py
@@ -0,0 +1,8 @@
+# address.py
+class Address:
+    def __init__(self, street1, city, state, zip):
+        self.street1 = street1
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.county = county
